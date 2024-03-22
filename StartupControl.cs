7fe20f751f5e7c81a8b73fb986129f486eeb6386
matbin532 <matbin@ktu.lt>
2024-03-22 16:11:45 +0200

    Add running at startup

---
+++ StartupControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Avalonia.Input;
+using Microsoft.Win32;
+
+namespace NotABook
+{
+    internal class StartupControl
+    {
+        bool enabled = false;
+        
+        private void SetStartup(bool enabled)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if (enabled) key.SetValue("NotABook", System.Reflection.Assembly.GetExecutingAssembly().Location);
+            else key.DeleteValue("NotABook", false);
+        }
+    }
+}
--- StorageController.cs
+++ StorageController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using Newtonsoft.Json;
 
 namespace NotABook
